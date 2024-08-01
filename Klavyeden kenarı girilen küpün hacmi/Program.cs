01a106453b71c30f8c0e86d86113df7038296cf0
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:53:46 +0300

    Add project files.

---
+++ Klavyeden kenarı girilen küpün hacmi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_kenarı_girilen_küpün_hacmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Formül : a*a*a
+
+            Console.WriteLine("Küpün bir kenarını girin");
+            int a = Int32.Parse(Console.ReadLine());
+
+            int hacim = a * a * a;
+
+            Console.WriteLine("Küpün hacmi =" + hacim);
+            Console.ReadLine();
+
+        }
+    }
+}
